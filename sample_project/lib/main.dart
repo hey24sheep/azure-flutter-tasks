b041c069cac0d5c728abf1e5f5ef163ee053c523
hey24sheep <hey24sheep@gmail.com>
2020-07-09 13:21:55 +0530

    removed deprecated code in sample project

--- sample_project/lib/main.dart
+++ sample_project/lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             new Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
