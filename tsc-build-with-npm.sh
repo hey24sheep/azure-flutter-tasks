8ab5871fc6cdd6858beac1bd512bfea8256be2b5
hey24sheep <hey24sheep@gmail.com>
2021-07-04 20:59:32 +0530

    help script

---
+++ tsc-build-with-npm.sh
@@ -0,0 +1,18 @@
+# Build TS
+npm install -g typescript
+cd ./tasks/test
+npm install
+tsc 
+cd ../install
+npm install
+tsc
+cd ../build
+npm install
+tsc 
+cd ../analyze
+npm install
+tsc 
+cd ../command
+npm install
+tsc 
+cd ../../
