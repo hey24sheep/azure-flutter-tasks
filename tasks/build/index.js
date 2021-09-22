968b2023fa724812f677beda1eb92e59c992d645
hey24sheep <hey24sheep@gmail.com>
2021-09-22 14:49:44 +0530

    null check for build number fixes #11

--- tasks/build/index.js
+++ tasks/build/index.js
@@ -33,9 +33,13 @@ function main() {
         let debugMode = task.getBoolInput('debugMode', false);
         let buildName = task.getInput('buildName', false);
         let buildNumber = task.getInput('buildNumber', false);
-        let buildNumberSplits = buildNumber.split('.');
-        // handle decimal, if buildNumber is like "1234.1"
-        buildNumber = buildNumberSplits[0];
+        if (buildNumber) {
+            let buildNumberSplits = buildNumber.split('.');
+            if (buildNumberSplits) {
+                // handle decimal, if buildNumber is like "1234.1"
+                buildNumber = buildNumberSplits[0];
+            }
+        }
         let buildFlavour = task.getInput('buildFlavour', false);
         let entryPoint = task.getInput('entryPoint', false);
         let dartDefine = task.getInput('dartDefine', false);
--- tasks/build/index.ts
+++ tasks/build/index.ts
@@ -26,9 +26,13 @@ async function main(): Promise<void> {
     let debugMode = task.getBoolInput('debugMode', false);
     let buildName = task.getInput('buildName', false);
     let buildNumber = task.getInput('buildNumber', false);
-    let buildNumberSplits = buildNumber.split('.');
-    // handle decimal, if buildNumber is like "1234.1"
-    buildNumber = buildNumberSplits[0]; 
+    if (buildNumber) {
+        let buildNumberSplits = buildNumber.split('.');
+        if (buildNumberSplits) {
+            // handle decimal, if buildNumber is like "1234.1"
+            buildNumber = buildNumberSplits[0];
+        }
+    }
     let buildFlavour = task.getInput('buildFlavour', false);
     let entryPoint = task.getInput('entryPoint', false);
     let dartDefine = task.getInput('dartDefine', false);
