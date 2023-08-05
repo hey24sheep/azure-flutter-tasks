e13b73ee62cee0f3cf978dd7bb368fbd7815ff50
hey24sheep <hey24sheep@gmail.com>
2023-08-05 15:27:56 +0530

    more tests

--- sample_project/test/api_dummy_test.dart
+++ sample_project/test/api_dummy_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:sample_project/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Api Dummy Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
@@ -26,8 +26,8 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
-  testWidgets('Other test', (WidgetTester tester) async {});
-  testWidgets('And another test', (WidgetTester tester) async {
-    //throw Error();
+
+  testWidgets('failure api dummy test', (WidgetTester tester) async {
+    // throw Error();
   });
 }
